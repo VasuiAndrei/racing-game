46a3b65d8bb389b4280fca98da004868eab4f6f8
Vasui Andrei <vasuiandrei00@gmail.com>
2023-02-13 13:49:53 +0200

    added Car class; studied variables

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,64 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Car carReference = new Car();
+        carReference.name = "Ferarri";
+        carReference.maxSpeed = 300;
+        carReference.fuellevel = 60;
+        carReference.mileage = 12.5;
+        carReference.totalTraveledDistance = 0;
+        carReference.damaged = true;
+        carReference.doorCount = 1;
+        carReference.color = "Red";
+
+        System.out.println("Propierties of car " + carReference.name);
+        System.out.println("Max speed: " + carReference.maxSpeed);
+        System.out.println("Fuel level: " + carReference.fuellevel);
+        System.out.println("Mileage: " + carReference.mileage);
+        System.out.println("Total travel distance: " + carReference.totalTraveledDistance );
+        System.out.println("Damaged: " + carReference.damaged);
+        System.out.println("Door Count: " + carReference.doorCount);
+        System.out.println("Color: " + carReference.color);
+        System.out.println(carReference);
+
+        Car car2 = new Car();
+        car2.name = "Renault";
+        car2.maxSpeed = 250;
+        car2.fuellevel = 70;
+        car2.mileage = 12;
+        car2.totalTraveledDistance = 100;
+        car2.damaged = false;
+        car2.doorCount = 3;
+        car2.color = "Black";
+
+        System.out.println("---------------------------------");
+        System.out.println("Propierties of car " + car2.name);
+        System.out.println("Max speed: " + car2.maxSpeed);
+        System.out.println("Fuel level: " + car2.fuellevel);
+        System.out.println("Mileage: " + car2.mileage);
+        System.out.println("Total travel distance: " + car2.totalTraveledDistance );
+        System.out.println("Damaged: " + car2.damaged);
+        System.out.println("Door Count: " + car2.doorCount);
+        System.out.println("Color: " + car2.color);
+
+        System.out.println("Initial name for car1: " + carReference.name);
+        carReference.name = "Audi";
+        System.out.println("Changed name for car1: " + carReference.name);
+
+        carReference = new Car();
+        System.out.println("Name of new care: " + carReference.name);
+        carReference.name = "BMW";
+
+        carReference = car2;
+        System.out.println("Name of car referenced from 2 variables. CarReference: " + carReference.name);
+        System.out.println("Name of car referenced from 2 variables. Car2: " + car2.name);
+
+        car2.name = "Dacia";
+        System.out.println("Updated name of car referenced from 2 variables. CarReference: " + carReference.name);
+        System.out.println("Updated name of car referenced from 2 variables. Car2: " + car2.name);
+
+        car2 = null;
+
+        System.out.println(carReference.name);
     }
 }
---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Car {
+    String name;
+    double fuellevel;
+    double mileage;
+    double totalTraveledDistance;
+    double maxSpeed;
+
+    int doorCount;
+    boolean damaged;
+    String color;
+}
