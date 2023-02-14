749b09fba775a83d4f6d9c9d65dff017ccb63242
Vasui Andrei <vasuiandrei00@gmail.com>
2023-02-14 12:15:06 +0200

    declaring methods

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -10,4 +10,26 @@ public class Vehicle {
 
     boolean damaged;
     String color;
+
+    public double accelerate(double speed, double durationInHours) {
+        // Ferrari is accelerating with 100 km/h for 1h
+        System.out.println(name + "is accelerating with " + speed + "km/h for " + durationInHours + "h");
+
+        // local variables
+        double distance = speed * durationInHours;
+
+        totalTraveledDistance = totalTraveledDistance + distance;
+
+        // same result as the statement above
+//        totalTraveledDistance += distance;
+
+        System.out.println("Total traveled distance for vehicle " + name + ": " + totalTraveledDistance);
+
+        double usedFuel = distance * mileage / 100;
+
+        fuellevel -= usedFuel;
+        System.out.println("Remaining fuel for vehicle " + name + ": " + fuellevel);
+
+        return distance;
+    }
 }
