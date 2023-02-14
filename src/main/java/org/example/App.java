f17f6d852efa314bb1171a97064e28dd51cd6366
Vasui Andrei <vasuiandrei00@gmail.com>
2023-02-14 16:02:06 +0200

    invoking methods

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -60,24 +60,28 @@ public class App
         System.out.println("Door Count: " + car2.doorCount);
         System.out.println("Color: " + car2.color);
 
-        System.out.println("Initial name for car1: " + carReference.name);
-        carReference.name = "Audi";
-        System.out.println("Changed name for car1: " + carReference.name);
+        double distanceForCar1 = carReference.accelerate(60, 1);
 
-        carReference = new Car();
-        System.out.println("Name of new care: " + carReference.name);
-        carReference.name = "BMW";
+        double distanceForCar2 = car2.accelerate(100, 1);
 
-        carReference = car2;
-        System.out.println("Name of car referenced from 2 variables. CarReference: " + carReference.name);
-        System.out.println("Name of car referenced from 2 variables. Car2: " + car2.name);
-
-        car2.name = "Dacia";
-        System.out.println("Updated name of car referenced from 2 variables. CarReference: " + carReference.name);
-        System.out.println("Updated name of car referenced from 2 variables. Car2: " + car2.name);
-
-        car2 = null;
-
-        System.out.println(carReference.name);
+//        System.out.println("Initial name for car1: " + carReference.name);
+//        carReference.name = "Audi";
+//        System.out.println("Changed name for car1: " + carReference.name);
+//
+//        carReference = new Car();
+//        System.out.println("Name of new care: " + carReference.name);
+//        carReference.name = "BMW";
+//
+//        carReference = car2;
+//        System.out.println("Name of car referenced from 2 variables. CarReference: " + carReference.name);
+//        System.out.println("Name of car referenced from 2 variables. Car2: " + car2.name);
+//
+//        car2.name = "Dacia";
+//        System.out.println("Updated name of car referenced from 2 variables. CarReference: " + carReference.name);
+//        System.out.println("Updated name of car referenced from 2 variables. Car2: " + car2.name);
+//
+//        car2 = null;
+//
+//        System.out.println(carReference.name);
     }
 }
